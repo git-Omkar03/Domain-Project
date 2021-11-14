4b27a182b4861c544c6efba7a3130b130a3b3805
Omkar Patil <patil.omkar26@outlook.com>
2021-11-14 16:09:56 +0530

    SQLLite removed and add SQLServer

---
+++ API/Extensions/ApllicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extensions
+{
+    public static class ApllicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services , IConfiguration config)
+        {
+            services.AddScoped<ITokenServices, TokenService>();
+
+            services.AddDbContextPool<DataContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DatabaseConnection"));
+            });
+
+            return services;
+        }
+
+        
+    }
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class TokenService : ITokenServices
+    {
+        private readonly SymmetricSecurityKey key;
+        public TokenService(IConfiguration config)
+        {
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(AppUser user)
+        {
+            //Claims constitute the payload part of Json token here.
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            //structureOfJwt
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }   
+    }
+}
